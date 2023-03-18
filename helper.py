fe9e78f030d73d59ac2c62ceff5b4ce1d37f8c91
ronak-2002 <ronak.kalra@iitgn.ac.in>
2023-03-19 00:28:40 +0530

    Product page added

--- helper.py
+++ helper.py
@@ -1,4 +1,9 @@
 # HELPER FUNCTIONS
+import datetime
 
 def is_logged_in():
     pass
+
+def current_date():
+    x = datetime.datetime.now()
+    return (str(x.year) + '-' + str(x.strftime("%m")) + '-' + str(x.strftime("%d")))
