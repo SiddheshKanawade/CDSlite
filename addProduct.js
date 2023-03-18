499dea6e91af22c4e25458c675ec93eadeafbe83
Ayush Gupta <guptaayush@iitgn.ac.in>
2023-03-18 14:57:22 +0530

    Add Product page

---
+++ addProduct.js
@@ -0,0 +1,14 @@
+function displaynext() {
+    answer = document.getElementById("Merch").value;
+    document.getElementById(answer).style.display = "block";
+
+    if (answer == "Yes") { // hide the div that is not selected
+
+        document.getElementById('No').style.display = "none";
+
+    } else if (answer == "No") {
+
+        document.getElementById('Yes').style.display = "none";
+
+    }
+}
