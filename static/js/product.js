750a7243934856b41ddf6b95e9ed6f169def497c
SiddheshKanawade <siddhesh.kanawade@iitgn.ac.in>
2023-03-19 21:26:09 +0530

    add bid_buyer

---
+++ static/js/product.js
@@ -0,0 +1,28 @@
+const productImages = document.querySelectorAll("edit-img img"); 
+const productImageSlide = document.querySelector("card"); 
+
+let activeImageSlide = 0; 
+
+productImages.forEach((item, i) => { 
+    item.addEventListener('click', () => { 
+        productImages[activeImageSlide].classList.remove('active'); 
+        item.classList.add('active'); 
+        productImageSlide.style.backgroundImage = `url('${item.src}')`; 
+        activeImageSlide = i; 
+    })
+})
+
+const sizeBtns = document.querySelectorAll('.size_button'); 
+let checkedBtn = 0; 
+
+sizeBtns.forEach((item, i) => { 
+    item.addEventListener('click', () => { 
+        sizeBtns[checkedBtn].classList.remove('check'); 
+        item.classList.add('check'); 
+        checkedBtn = i; 
+    })
+})
+
+function addToCart() {
+    window.location.href = 'cart.html';
+  }
